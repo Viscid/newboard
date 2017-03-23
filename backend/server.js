b920951ebd294174c71878f1fedda92809690918
Mark Gillis <viscid@gmail.com>
2017-03-23 00:02:33 -0400

    Adds morgan logging

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 var express = require('express')
-
 var app = express()
-
 var bodyParser = require('body-parser')
+var morgan = require('morgan')
 
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use(bodyParser.json())
+app.use(morgan('dev'))
 
 var models = require('./models')
 
