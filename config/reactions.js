ca24dff44f410e83895903d4da1ab8dbc7d2fc43
Mark Gillis <viscid@gmail.com>
2017-05-30 02:10:43 -0400

    Remove template strings from reactions

--- config/reactions.js
+++ config/reactions.js
@@ -1,28 +1,28 @@
 module.exports = [
   { name: 'Nod',
-    describe: function(subject, object) { return `${subject} nodded.` }
+    describe: function(subject, object) { return subject + ' nodded.' }
   }, 
   { name: 'Shrug',
-    describe: function(subject, object) { return `${subject} gave a shrug.` } 
+    describe: function(subject, object) { return subject + ' gave a shrug.' } 
   },
   {
     name: 'Chuckle',
-    describe: function(subject, object) { return `${subject} let out a chuckle.` }
+    describe: function(subject, object) { return subject + ' let out a chuckle.' }
   },
   {
     name: 'Spit',
-    describe: function(subject, object) { return `${subject} spat.` }
+    describe: function(subject, object) { return subject + ' spat.' }
   },
   {
     name: 'Hiss',
-    describe: function(subject, object) { return `${subject} hissed.` }
+    describe: function(subject, object) { return subject + ' hissed.' }
   },
   {
     name: 'Roll',
-    describe: function(subject, object) { return `${subject} rolled around on the ground.` }
+    describe: function(subject, object) { return subject + ' rolled around on the ground.' }
   },
   {
     name: 'Hat Tip',
-    describe: function(subject, object) { return `${subject} tips his hat to ${object}.`}
+    describe: function(subject, object) { return subject + ' tips his hat to ' + object }
   }
 ]
